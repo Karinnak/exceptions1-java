726afbf1018e124ff174256fb5baf2fd63f8032c
Karina Ferreira <karinafigueredo.k.t.d@gmail.com>
2024-08-15 19:04:17 -0300

    Good solution

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -6,6 +6,8 @@ package model.entities;
 	import java.util.Date;
 	import java.util.concurrent.TimeUnit;
 
+    import model.exceptions.DomanException;
+
 
 	public class Reservation {
 
@@ -15,7 +17,10 @@ package model.entities;
 		
 		private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		public Reservation(Integer roomNumber, Date checkIn, Date checkOut)  {
+			 if(!checkOut.after(checkIn)) {
+				throw new DomanException ("Check-out date must be after check-in date");
+			}
 			this.roomNumber = roomNumber;
 			this.checkIn = checkIn;
 			this.checkOut = checkOut;
@@ -42,17 +47,17 @@ package model.entities;
 			return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 		}
 		
-		public String updateDates(Date checkIn, Date checkOut) {
+		public void updateDates(Date checkIn, Date checkOut) {
 		    Date now = new Date();
 			if(checkIn.before(now) || checkOut.before(now)) {
-				return "Reservation dates for update must be future dates.";
+				throw new DomanException("Reservation dates for update must be future dates.");
 			}
 		    if(!checkOut.after(checkIn)) {
-				return "Check-out date must be after check-in date";
+				throw new DomanException ("Check-out date must be after check-in date");
 			}
 			   this.checkIn = checkIn;
 			   this.checkOut = checkOut;
-			   return null;
+			   
 			   
 		}
 		
---
+++ src/model/exceptions/DomanException.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+public class DomanException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DomanException(String msg) {
+		 super(msg);
+	}
+
+}
